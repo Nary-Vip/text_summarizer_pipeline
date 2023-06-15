9c8519811fc1a830526264694063d01337d024c5
Nary-Vip <nary2vip@gmai.com>
2023-06-15 15:35:07 +0530

    Data validation modular coding completed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from textSummarizer.logging import logger
 from textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from textSummarizer.pipeline.stage_02_data_validation import DataValidationPipeline
 
 STAGE_NAME = "Data  Ingestion stage"
 
@@ -7,6 +8,13 @@ try:
     logger.info(f">>>> stage {STAGE_NAME} started <<<<")
     data_ingestion = DataIngestionTrainingPipeline()
     data_ingestion.main()
+    logger.info(f">>>> stage {STAGE_NAME} completed! <<<<")
+
+
+    STAGE_NAME = "Data Validation stage"
+    logger.info(f">>>> stage {STAGE_NAME} started <<<<")
+    data_validation = DataValidationPipeline()
+    data_validation.main()
     logger.info(f">>>> stage {STAGE_NAME} completed! <<<<")
 
 except Exception as e:
--- src/textSummarizer/config/configuration.py
+++ src/textSummarizer/config/configuration.py
@@ -1,6 +1,6 @@
 from textSummarizer.constants import *
 from textSummarizer.utils.common import read_yaml, create_directories
-from textSummarizer.entity import DataIngestionConfig
+from textSummarizer.entity import DataIngestionConfig, DataValidationConfig
 
 class ConfigurationManager:
     def __init__(self, config_path=CONFIG_FILE_PATH, params_path=PARAMS_FILE_PATH):
@@ -19,4 +19,15 @@ class ConfigurationManager:
             source_url=config.source_URL,
             local_data_file=config.local_data_file,
             unzip_dir=config.unzip_dir
+        )
+    
+    def get_data_validation(self) -> DataValidationConfig:
+        config = self.config["data_validation"]
+
+        create_directories([config.root_dir])
+        
+        return DataValidationConfig(
+            root_dir=Path(config.root_dir),
+            STATUS_FILE=config.STATUS_FILE,
+            ALL_REQUIRED_FILES=config.ALL_REQUIRED_FILES
         )
