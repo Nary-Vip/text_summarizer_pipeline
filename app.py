a58b14243f6837b8e1f1cb6446f4281d0c47cb13
Nary-Vip <nary2vip@gmai.com>
2023-06-18 12:45:22 +0530

    Prediction pipeline and fast API routes implemented

---
+++ app.py
@@ -0,0 +1,44 @@
+import os
+import sys
+from fastapi import FastAPI
+# from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
+from starlette.responses import RedirectResponse
+from fastapi.responses import Response
+from textSummarizer.pipeline.prediction import PredictionPipeline
+
+
+app = FastAPI()
+
+text:str = "What is text summarisation?"
+
+@app.get("/", tags=["authentication"])
+async def index():
+    return RedirectResponse(url="/docs/")
+
+@app.get("/check")
+async def check():
+    return Response("API is up and running!")
+
+@app.get("/train")
+async def train():
+    try:
+        os.system("python main.py")
+        return Response("Training completed successfully!")
+    
+    except Exception as e:
+        return Response(e)
+    
+@app.get("/predict")
+async def predict(text:str):
+    try:
+        prediction = PredictionPipeline()
+        summary = prediction.predict(text)
+        return summary
+    
+    except Exception as e:
+        return Response(e)
+    
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
